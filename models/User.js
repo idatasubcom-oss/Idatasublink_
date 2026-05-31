4a8806691d56bc2121d388cf25a32b6f4355fc52
idatasubcom-oss <idatasub.com@gmail.com>
2026-05-31 16:42:38 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,39 @@
+const mongoose = require("mongoose");
 
+const userSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  balance: {
+    type: Number,
+    default: 0
+  },
+
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
