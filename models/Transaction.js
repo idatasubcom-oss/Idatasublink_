5715c3c411c42b80dbdf1591c40f9cdbd069206f
idatasubcom-oss <idatasub.com@gmail.com>
2026-05-31 23:50:14 +0100

    Create Transaction.js

---
+++ models/Transaction.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: "Success",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Transaction", transactionSchema);
